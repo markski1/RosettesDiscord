df107b274ba7de3b0ce2049a1c3e729e040e1952
Juan Geido <me@markski.ar>
2023-10-10 00:57:44 -0300

    Introduce API controller for future stuff.

---
+++ Rosettes/Controllers/ApiController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rosettes.Controllers;
+
+[ApiController]
+[Route("rosettes-api")]
+public class ApiController : ControllerBase
+{
+    private readonly ILogger<ApiController> _logger;
+
+    public ApiController(ILogger<ApiController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("CheckAlive")]
+    public string CheckAlive()
+    {
+        return "Rosettes lives!";
+    }
+}
--- Rosettes/Program.cs
+++ Rosettes/Program.cs
@@ -1,4 +1,30 @@
 ﻿using Rosettes.Core;
 
+Task task = Task.Run(() => {
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.MapControllers();
+
+    app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true)
+                .AllowCredentials());
+
+    app.Run();
+});
+
 // Initialize bot
 await Global.RosettesMain.MainAsync();
