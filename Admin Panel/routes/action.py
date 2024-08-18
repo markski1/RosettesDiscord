e678e12387e9225ff845a0b98a36a6227e0338e8
Juan G. <me@markski.ar>
2024-08-18 05:28:51 -0300

    Bit of refactoring on webpanel

---
+++ Admin Panel/routes/action.py
@@ -0,0 +1,52 @@
+from flask import Blueprint, render_template, request
+from flask_login import login_required
+
+from utils.db_getters import set_server_settings
+from utils.miscfuncs import htmx_check, ownership_required
+
+panel_bp = Blueprint("action", __name__, url_prefix="/action")
+
+
+@panel_bp.route("/")
+@login_required
+@htmx_check
+def index():
+    return "Nothing here but us sneps!"
+
+
+@panel_bp.post("/<int:server_id>/update-settings")
+@login_required
+@ownership_required
+@htmx_check
+def post_settings(is_htmx, server_id):
+    try:
+        msgparse = abs(int(request.form["msgparse"]))
+        minigame = abs(int(request.form["minigame"]))
+        gambling = abs(int(request.form["gambling"]))
+        announce = abs(int(request.form["announce"]))
+    except:
+        return "Invalid parameters."
+
+    if int(msgparse) > 1 or int(minigame) > 1 or int(gambling) > 1 or int(announce) > 2:
+        return "Invalid parameters."
+
+    new_settings = f"{msgparse}1{gambling}1{minigame}{announce}1111"
+    set_server_settings(server_id, new_settings)
+    return render_template("prompts/success.html", is_htmx=is_htmx, message="Settings updated successfully.")
+
+
+@panel_bp.post("/<int:server_id>/create-cmd")
+@login_required
+@ownership_required
+@htmx_check
+def post_settings(is_htmx, server_id):
+    try:
+        name = abs(int(request.form["name"]))
+        description = abs(int(request.form["description"]))
+        cmd_type = abs(int(request.form["type"]))
+        content = abs(int(request.form["content"]))
+    except:
+        return "Invalid parameters."
+
+    return "Not yet implemented."
+
--- Admin Panel/routes/panel.py
+++ Admin Panel/routes/panel.py
@@ -1,9 +1,6 @@
-from functools import wraps
+from flask import Blueprint, render_template, request
+from flask_login import login_required, current_user
 
-from flask import Blueprint, render_template, request, redirect
-from flask_login import login_required, current_user, login_user
-
-from core.session import attempt_login
 from utils.db_getters import get_owned_servers, get_user_data, get_server_data, set_server_settings, get_server_commands
 from utils.miscfuncs import htmx_check, ownership_required
 
@@ -38,27 +35,6 @@ def settings(is_htmx, server_id):
 
     return render_template("settings.html", is_htmx=is_htmx, server=server,
                            msgparse=msgparse, minigame=minigame, gambling=gambling, announce=announce)
-
-
-@panel_bp.post("/<int:server_id>/update-settings")
-@login_required
-@ownership_required
-@htmx_check
-def post_settings(is_htmx, server_id):
-    try:
-        msgparse = abs(int(request.form["msgparse"]))
-        minigame = abs(int(request.form["minigame"]))
-        gambling = abs(int(request.form["gambling"]))
-        announce = abs(int(request.form["announce"]))
-    except:
-        return "Invalid parameters."
-
-    if int(msgparse) > 1 or int(minigame) > 1 or int(gambling) > 1 or int(announce) > 2:
-        return "Invalid parameters."
-
-    new_settings = f"{msgparse}1{gambling}1{minigame}{announce}1111"
-    set_server_settings(server_id, new_settings)
-    return render_template("prompts/success.html", is_htmx=is_htmx, message="Settings updated successfully.")
 
 
 @panel_bp.get("/<int:server_id>/roles")
